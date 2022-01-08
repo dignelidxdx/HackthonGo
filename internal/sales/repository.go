24ce80a04543acb87fb19993e25c40405a8b8849
dignelidxdx <digneli.davila@mercadolibre.com>
2022-01-08 14:30:00 -0300

    added entity sale repository and service

--- internal/sales/repository.go
+++ internal/sales/repository.go
@@ -1,1 +1,60 @@
 package internal
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/dignelidxdx/HackthonGo/internal/models"
+	"github.com/dignelidxdx/HackthonGo/pkg/db"
+)
+
+type SaleRepository interface {
+	Save(sale models.Sale) (models.Sale, error)
+	SaveFile(sales []models.Sale) error
+}
+
+type saleRepository struct {
+}
+
+func NewSaleRepository() SaleRepository {
+	return &saleRepository{}
+}
+
+func (r *saleRepository) Save(sale models.Sale) (models.Sale, error) {
+	db := db.StorageDB
+
+	stmt, err := db.Prepare("INSERT INTO sales(description,price) VALUES(?,?)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+	var result sql.Result
+	result, err = stmt.Exec(sale.Quantity, sale.Product)
+	if err != nil {
+		return models.Sale{}, err
+	}
+	idCreado, _ := result.LastInsertId()
+	sale.ID = int(idCreado)
+
+	return sale, nil
+}
+
+func (r *saleRepository) SaveFile(sales []models.Sale) error {
+	db := db.StorageDB
+
+	for _, sale := range sales {
+
+		stmt, err := db.Prepare("INSERT INTO sales(last_name,first_name,`condition`) VALUES(?,?,?)")
+		if err != nil {
+			log.Fatal(err)
+			return err
+		}
+		defer stmt.Close()
+
+		_, err = stmt.Exec(sale.Quantity, sale.Product)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
--- internal/sales/service.go
+++ internal/sales/service.go
@@ -1,1 +1,34 @@
 package internal
+
+import "github.com/dignelidxdx/HackthonGo/internal/models"
+
+type SaleService interface {
+	SaveSale(models.Sale) (models.Sale, error)
+	SaveFile([]models.Sale) error
+}
+
+type saleService struct {
+	repository SaleRepository
+}
+
+func NewSaleService(repository SaleRepository) SaleService {
+	return &saleService{repository: repository}
+}
+
+func (s *saleService) SaveSale(sale models.Sale) (models.Sale, error) {
+
+	sale, err := s.repository.Save(sale)
+	if err != nil {
+		return models.Sale{}, err
+	}
+	return sale, nil
+}
+
+func (s *saleService) SaveFile(sales []models.Sale) error {
+
+	err := s.repository.SaveFile(sales)
+	if err != nil {
+		return err
+	}
+	return nil
+}
