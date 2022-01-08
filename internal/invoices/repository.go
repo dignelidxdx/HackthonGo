4df816c3dc8493460ba6dea53295804f08bedf6b
dignelidxdx <digneli.davila@mercadolibre.com>
2022-01-08 14:28:48 -0300

    added invoice entity

--- internal/invoices/repository.go
+++ internal/invoices/repository.go
@@ -1,1 +1,61 @@
 package internal
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/dignelidxdx/HackthonGo/internal/models"
+	"github.com/dignelidxdx/HackthonGo/pkg/db"
+)
+
+type InvoiceRepository interface {
+	SaveFile(invoices []models.Invoice) error
+	Save(invoice models.Invoice) (models.Invoice, error)
+}
+
+type invoiceRepository struct {
+}
+
+func NewInvoiceRepository() InvoiceRepository {
+	return &invoiceRepository{}
+}
+
+func (r *invoiceRepository) SaveFile(invoices []models.Invoice) error {
+	db := db.StorageDB
+
+	for _, c := range invoices {
+
+		// TODO: cambiar datos del insert
+		stmt, err := db.Prepare("INSERT INTO invoices(last_name,first_name,`condition`) VALUES(?,?,?)")
+		if err != nil {
+			log.Fatal(err)
+			return err
+		}
+		defer stmt.Close()
+
+		_, err = stmt.Exec(c.Total, c.Customer, c.Datetime)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (r *invoiceRepository) Save(invoice models.Invoice) (models.Invoice, error) {
+	db := db.StorageDB
+
+	stmt, err := db.Prepare("INSERT INTO invoices(last_name,first_name,`condition`) VALUES(?,?,?)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+	var result sql.Result
+	result, err = stmt.Exec(invoice.Total, invoice.Customer, invoice.Datetime)
+	if err != nil {
+		return models.Invoice{}, err
+	}
+	idCreado, _ := result.LastInsertId()
+	invoice.ID = int(idCreado)
+
+	return invoice, nil
+}
--- internal/invoices/service.go
+++ internal/invoices/service.go
@@ -1,1 +1,34 @@
 package internal
+
+import "github.com/dignelidxdx/HackthonGo/internal/models"
+
+type InvoiceService interface {
+	SaveInvoice(models.Invoice) (models.Invoice, error)
+	SaveFile([]models.Invoice) error
+}
+
+type invoiceService struct {
+	repository InvoiceRepository
+}
+
+func NewInvoiceService(repository InvoiceRepository) InvoiceService {
+	return &invoiceService{repository: repository}
+}
+
+func (s *invoiceService) SaveInvoice(invoice models.Invoice) (models.Invoice, error) {
+
+	invoice, err := s.repository.Save(invoice)
+	if err != nil {
+		return models.Invoice{}, err
+	}
+	return invoice, nil
+}
+
+func (s *invoiceService) SaveFile(invoices []models.Invoice) error {
+
+	err := s.repository.SaveFile(invoices)
+	if err != nil {
+		return err
+	}
+	return nil
+}
