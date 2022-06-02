5bf2ae4fb308b4c7df7eac9c0042342f637b8eed
dignelidxdx <digneli.davila@mercadolibre.com>
2022-06-02 15:40:59 -0300

    change circuit breaker

--- src/api/main.go
+++ src/api/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
-	circuitBreaker "github.com/dignelidxdx/HackthonGo/pkg/lib/circuitBreaker"
 	handlerE "github.com/dignelidxdx/HackthonGo/wrapper/adapter/in/handler"
 	"github.com/dignelidxdx/HackthonGo/wrapper/adapter/out/owner"
 	"github.com/dignelidxdx/HackthonGo/wrapper/adapter/out/persistence"
 	"github.com/dignelidxdx/HackthonGo/wrapper/application/service"
+
+	circuitBreaker "github.com/dignelidxdx/HackthonGo/pkg/lib/circuitBreaker"
+
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
@@ -21,7 +23,7 @@ func main() {
 	repo, _ := persistence.NewEmployeeRepository()
 	newClient := owner.NewClient("")
 	circuitBreaker := circuitBreaker.NewCircuitBreaker()
-	service := service.NewEmployeeService(repo, newClient)
+	service := service.NewEmployeeService(repo, newClient, circuitBreaker)
 	controller := handlerE.NewEmployee(service)
 
 	router.GET("/employees", controller.GetAll())
